352fa940ed9da1e660551236a843f4b6bc5041cd
YUdintsevaAK <YUdintsevaAK@DOM201.LOCAL>
2021-11-17 12:04:07 +0700

    Добавлена реализация BooksController и отображение компонента через view

--- BootstrapTest/Controllers/BooksController.cs
+++ BootstrapTest/Controllers/BooksController.cs
@@ -11,9 +11,37 @@ namespace BootstrapTest.Controllers
     {
         private readonly BooksCatalog _booksCatalog;
 
+        public BooksController()
+        {
+            _booksCatalog = new BooksCatalog();
+
+            _booksCatalog.AddBook(new Book
+            {
+                Title = "Цветы для Элджернона",
+                Author = "Дэниел Киз",
+                Genre = "фантастика, драма, психология",
+                InventoryNumber = Guid.NewGuid().ToString(), 
+                ISBN = "9780606340182",
+                YearOfEdition = "1959"
+            });
+
+            _booksCatalog.AddBook(new Book
+            {
+                Title = "Пркосновение",
+                Author = "Дэниел Киз",
+                Genre = "фантастика, драма, психология",
+                InventoryNumber = Guid.NewGuid().ToString(),
+                ISBN = "9780709121435",
+                YearOfEdition = "1968"
+            });
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var books = _booksCatalog.GetAll();
+            ViewBag.Title = "Catalog Book";
+
+            return View(books);
         }
     }
 }
